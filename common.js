a9afc17fbb4d61bc042f4e1d8bb767173366c0f3
V Yashwanth Kumar <23eg105t62@anurag.edu.in>
2025-12-29 21:57:43 +0530

    Create common.js

---
+++ common.js
@@ -0,0 +1,26 @@
+// common.js
+
+// Navbar scroll effect
+document.addEventListener('DOMContentLoaded', function() {
+    const navbar = document.querySelector('.navbar');
+    
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 50) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    });
+    
+    // Activate current page link
+    const currentPage = window.location.pathname.split('/').pop();
+    const navLinks = document.querySelectorAll('.nav-link');
+    
+    navLinks.forEach(link => {
+        if (link.getAttribute('href') === currentPage) {
+            link.classList.add('active');
+        } else if (currentPage === '' || currentPage === 'index.html') {
+            document.querySelector('.nav-link[href="index.html"]').classList.add('active');
+        }
+    });
+});
